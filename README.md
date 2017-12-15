a2fd87260ef3fe87f7b74c4846640e74352d6452
fadlibotax <fadlibotax@outlook.co.id>
2017-12-15 11:48:15 +0700

    pull data from mysql to json and use datatable to display the data

--- README.md
+++ README.md
@@ -2,11 +2,17 @@
 
 Ini adalah repostitory dari tugas-tugas mata kuliah Pemrograman Berbasis Web 4KA20. Ini adalah repostitory dari tugas-tugas mata kuliah Pemrograman Berbasis Web 4KA20.
 
-DataTables with Ajax: https://datatables.net/examples/ajax/objects.html
-
 ## Tugas 1 Tabel
 Struktur Tabel
 * ID (primary key, int)
 * nama(varchar)
 * kelas(varchar)
 * jurusan(varchar)
+
+DataTables with Ajax:
+https://datatables.net/examples/ajax/objects.html
+https://datatables.net/manual/ajax
+
+Referensi:
+http://www.kodingmadesimple.com/2015/01/convert-mysql-to-json-using-php.html
+
--- tugas1tabel/config.php
+++ tugas1tabel/config.php
@@ -1,10 +1,21 @@
 <?php
 
-define (DB_USER, "root");
-define (DB_PASSWORD, "");
-define (DB_DATABASE, "db_kuliahweb");
-define (DB_HOST, "localhost");
+//open koneksi to mysql db
+$koneksi = mysqli_connect("localhost","root","","db_kuliahweb") or die("Error " . mysqli_error($koneksi));
 
-$koneksi = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
+//fetch table rows from mysql db
+$sql = "select * from tb_data";
+$result = mysqli_query($koneksi, $sql) or die("Error in Selecting " . mysqli_error($koneksi));
+
+//membuat array output berupa json
+$data = array();
+while($row =mysqli_fetch_assoc($result))
+{
+    $data[] = $row;
+}
+echo json_encode($data);
+
+//close the db koneksi
+mysqli_close($koneksi);
 
 ?>
